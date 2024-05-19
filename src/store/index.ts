9c3fa23c68fd57c536ab6cb7ee319ffcca819971
mcge <ge942812@gmail.com>
2024-05-19 16:33:03 +0800

    :sparkles: feat(store): add redux
    add redux manager store.

---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+/**
+ *   Copyright (C) 2024 Mcge. All rights reserved.
+ *
+ * File:           index.ts
+ * Author:         mcge
+ * Email:          <mcgeq@outlook.com>
+ * Description:
+ * Create Date:    2024-05-19 16:29:02
+ * Last Modified:  2024-05-19 16:32:49
+ *
+ */
+
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
