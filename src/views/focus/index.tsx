8d642942b238dfd8b6fec8185494cce162301c52
mcge <ge942812@gmail.com>
2024-05-19 15:18:35 +0800

    :sparkles: feat(views): add views components
    add discover download mine focus ed components.

---
+++ src/views/focus/index.tsx
@@ -0,0 +1,23 @@
+/**
+ *   Copyright (C) 2024 Mcge. All rights reserved.
+ *
+ * File:           index.tsx
+ * Author:         mcge
+ * Email:          <mcgeq@outlook.com>
+ * Description:
+ * Create Date:    2024-05-19 14:27:12
+ * Last Modified:  2024-05-19 14:27:12
+ *
+ */
+
+import type { FC, ReactNode } from 'react';
+
+interface IProps {
+  children?: ReactNode;
+}
+
+const Focus: FC<IProps> = () => {
+  return <div>Focus</div>;
+};
+
+export default Focus;
